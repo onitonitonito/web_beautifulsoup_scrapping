be59ed034c56b9de38e7ad15c89e125abc400ded
What Onito3 on Earth! <nitt0x0@gmail.com>
2018-09-11 01:12:22 +0900

    1.2-html 2 md - tomd

---
+++ adsp_training/html_to_text_test.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import tomd as td
+import html2text as ht
+from pprint import pprint
+
+
+HOME_NAME = "web_beautifulsoup_scrapping"
+DIRS = os.path.dirname(__file__).partition(HOME_NAME)
+HOME_DIR = DIRS[0] + DIRS[1] + "\\"
+
+sys.path.append(HOME_DIR)
+
+# FILE_NAME = "01_fruit.html" # N.G
+FILE_NAME = "html_to_text_sample.html"    # O.K
+
+FILE_W_DIR = HOME_DIR + "\\_static\\" + FILE_NAME
+
+with open(FILE_W_DIR, 'r', encoding='utf-8') as f:
+    html = f.read()
+
+
+# 간단한 HTML 에 대해서 Markdown 텍스트로 변환
+# 그런데, 복잡한 것은 에러 남.
+
+a = td.Tomd(html).markdown
+print(a)

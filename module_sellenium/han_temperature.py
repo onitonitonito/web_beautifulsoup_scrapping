ff850d555441c4485f4636640c96cb41594e1bb6
Kay Suparx <nitt0x0@gmail.com>
2020-01-23 16:34:11 +0900

    6.1-han_temperature

---
+++ module_sellenium/han_temperature.py
@@ -0,0 +1,16 @@
+http://www.koreawqi.go.kr/wQDDRealTotalDataList_D.wq?
+item_id=M69&
+action_type=L&
+action_type_seq=1&
+auto_flag=&
+auto_site_id=S01007&
+search_data_type=1&
+search_flag2=1&
+river_id=R01&
+site_id=%27S01004%27&
+site_name=%B1%B8%B8%AE&
+search_interval=HOUR&
+search_date_from=${date}&
+search_date_to=${date}&
+order_type_1=MSR_DATE&
+order_type_2=ASC
